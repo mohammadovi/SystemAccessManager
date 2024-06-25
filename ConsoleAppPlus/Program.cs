c3fa87a4f5819ac576f5c58cd50a658ab5341767
Mohammad ahmadi <mohammadahmadidv@gmail.com>
2024-06-25 22:04:44 +0330

    Revert "#000003_HideTaskBar"
    This reverts commit cc903eecc8d486771bc33ec8295c68d3d2b354d7.

--- ConsoleAppPlus/Program.cs
+++ ConsoleAppPlus/Program.cs
@@ -11,17 +11,13 @@ using Microsoft.Win32;
 using System.Threading;
 using Microsoft.Win32.TaskScheduler;
 using System.Reflection;
-
 namespace SystemAccessManager.ConsoleAppPlus
 {
     public class Program
     {
         public const string RegistryKeyName = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
         public const string RegistryValueName = "DisableTaskMgr";
-        public const string StartMenuRegistryKeyName = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer";
-        public const string StartMenuRegistryValueName = "NoStartMenuMorePrograms";
-        public const string TaskbarRegistryValueName = "NoTrayItemsDisplay";
-        public const string AppName = "MyApp"; // Your app name for use in the registry
+        public const string AppName = "MyApp"; // نام برنامه شما برای استفاده در رجیستری
 
         /// <summary>
         /// Main entry point of the application.
@@ -34,19 +30,11 @@ namespace SystemAccessManager.ConsoleAppPlus
 
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Choose your option:");
-            Console.WriteLine("1. Choose custom settings for Task Manager");
-            Console.WriteLine("2. Use predefined settings for Task Manager");
-            Console.WriteLine("3. Disable Start Menu");
-            Console.WriteLine("4. Enable Start Menu");
-            Console.WriteLine("5. Hide Start Menu");
-            Console.WriteLine("6. Unhide Start Menu");
-            Console.WriteLine("7. Disable Taskbar");
-            Console.WriteLine("8. Enable Taskbar");
-            Console.WriteLine("9. Hide Taskbar");
-            Console.WriteLine("10. Unhide Taskbar");
+            Console.WriteLine("1. Choose custom settings");
+            Console.WriteLine("2. Use predefined settings");
             Console.WriteLine("Type 'EXIT' to quit.");
 
-            // Check for first run and add to startup
+            // بررسی برای اولین اجرا و افزودن به startup
             bool isFirstRun = CheckFirstRun();
 
             if (isFirstRun)
@@ -66,7 +54,7 @@ namespace SystemAccessManager.ConsoleAppPlus
                     break;
                 }
 
-                if (int.TryParse(input, out int choice) && (choice >= 1 && choice <= 10))
+                if (int.TryParse(input, out int choice) && (choice == 1 || choice == 2))
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("Processing...");
@@ -88,67 +76,11 @@ namespace SystemAccessManager.ConsoleAppPlus
                         Console.ResetColor();
                         SetTaskManagerStatus(1); // Disables Task Manager
                     }
-                    else if (choice == 3)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Magenta;
-                        Console.WriteLine("Disabling Start Menu...");
-                        Console.ResetColor();
-                        SetStartMenuStatus(1);
-                    }
-                    else if (choice == 4)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Magenta;
-                        Console.WriteLine("Enabling Start Menu...");
-                        Console.ResetColor();
-                        SetStartMenuStatus(0);
-                    }
-                    else if (choice == 5)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Magenta;
-                        Console.WriteLine("Hiding Start Menu...");
-                        Console.ResetColor();
-                        HideStartMenu(true);
-                    }
-                    else if (choice == 6)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Magenta;
-                        Console.WriteLine("Unhiding Start Menu...");
-                        Console.ResetColor();
-                        HideStartMenu(false);
-                    }
-                    else if (choice == 7)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Magenta;
-                        Console.WriteLine("Disabling Taskbar...");
-                        Console.ResetColor();
-                        SetTaskbarStatus(1);
-                    }
-                    else if (choice == 8)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Magenta;
-                        Console.WriteLine("Enabling Taskbar...");
-                        Console.ResetColor();
-                        SetTaskbarStatus(0);
-                    }
-                    else if (choice == 9)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Magenta;
-                        Console.WriteLine("Hiding Taskbar...");
-                        Console.ResetColor();
-                        HideTaskbar(true);
-                    }
-                    else if (choice == 10)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Magenta;
-                        Console.WriteLine("Unhiding Taskbar...");
-                        Console.ResetColor();
-                        HideTaskbar(false);
-                    }
                 }
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Invalid input. Please enter a number between 1 and 10, or EXIT.");
+                    Console.WriteLine("Invalid input. Please enter 1, 2, or EXIT.");
                     Console.ResetColor();
                 }
             }
@@ -204,140 +136,6 @@ namespace SystemAccessManager.ConsoleAppPlus
         }
 
         /// <summary>
-        /// Sets the Start Menu status.
-        /// </summary>
-        /// <param name="status">The status to set (0 for enable, 1 for disable).</param>
-        public static void SetStartMenuStatus(int status)
-        {
-            try
-            {
-                ShowProgressBar(true);
-
-                using (RegistryKey key = Registry.CurrentUser.CreateSubKey(StartMenuRegistryKeyName))
-                {
-                    key.SetValue(StartMenuRegistryValueName, status, RegistryValueKind.DWord);
-                }
-
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine($"Start Menu status set to: {(status == 0 ? "Enabled" : "Disabled")}");
-            }
-            catch (Exception ex)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Error: {ex.Message}");
-                ShowProgressBar(false);
-            }
-            finally
-            {
-                Console.ResetColor();
-            }
-        }
-
-        /// <summary>
-        /// Hides or unhides the Start Menu.
-        /// </summary>
-        /// <param name="hide">True to hide, false to unhide.</param>
-        public static void HideStartMenu(bool hide)
-        {
-            try
-            {
-                ShowProgressBar(true);
-
-                using (RegistryKey key = Registry.CurrentUser.CreateSubKey(StartMenuRegistryKeyName))
-                {
-                    key.SetValue("NoStartMenu", hide ? 1 : 0, RegistryValueKind.DWord);
-                }
-
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine($"Start Menu {(hide ? "hidden" : "unhidden")}.");
-            }
-            catch (Exception ex)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Error: {ex.Message}");
-                ShowProgressBar(false);
-            }
-            finally
-            {
-                Console.ResetColor();
-            }
-        }
-
-        /// <summary>
-        /// Sets the Taskbar status.
-        /// </summary>
-        /// <param name="status">The status to set (0 for enable, 1 for disable).</param>
-        public static void SetTaskbarStatus(int status)
-        {
-            try
-            {
-                ShowProgressBar(true);
-
-                using (RegistryKey key = Registry.CurrentUser.CreateSubKey(StartMenuRegistryKeyName))
-                {
-                    key.SetValue(TaskbarRegistryValueName, status, RegistryValueKind.DWord);
-                }
-
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine($"Taskbar status set to: {(status == 0 ? "Enabled" : "Disabled")}");
-            }
-            catch (Exception ex)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Error: {ex.Message}");
-                ShowProgressBar(false);
-            }
-            finally
-            {
-                Console.ResetColor();
-
-
-            }
-        }
-
-        /// <summary>
-        /// Hides or unhides the Taskbar.
-        /// </summary>
-        /// <param name="hide">True to hide, false to unhide.</param>
-        public static void HideTaskbar(bool hide)
-        {
-            try
-            {
-                ShowProgressBar(true);
-
-                // Get the handle of the Taskbar window
-                IntPtr taskbarHandle = FindWindow("Shell_TrayWnd", null);
-                if (taskbarHandle != IntPtr.Zero)
-                {
-                    ShowWindow(taskbarHandle, hide ? SW_HIDE : SW_SHOW);
-                }
-
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine($"Taskbar {(hide ? "hidden" : "unhidden")}.");
-            }
-            catch (Exception ex)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Error: {ex.Message}");
-                ShowProgressBar(false);
-            }
-            finally
-            {
-                Console.ResetColor();
-            }
-        }
-
-        // PInvoke declarations
-        [DllImport("user32.dll")]
-        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-        [DllImport("user32.dll")]
-        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-        private const int SW_HIDE = 0;
-        private const int SW_SHOW = 5;
-
-        /// <summary>
         /// Shows a progress bar or spinner on the console.
         /// </summary>
         /// <param name="success">True if the operation succeeded; false otherwise.</param>
@@ -352,7 +150,7 @@ namespace SystemAccessManager.ConsoleAppPlus
                 for (int i = 0; i <= 50; i++)
                 {
                     Console.Write("=");
-                    Thread.Sleep(10); // Delay to show progress bar gradually
+                    Thread.Sleep(100); // Delay to show progress bar gradually
                 }
             }
             else
@@ -380,16 +178,16 @@ namespace SystemAccessManager.ConsoleAppPlus
         /// <returns>True if it's the first run; false otherwise.</returns>
         public static bool CheckFirstRun()
         {
-            // Check for the existence of the registry key
+            // چک کردن برای وجود کلید رجیستری
             using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true))
             {
                 if (key != null)
                 {
                     string value = (string)key.GetValue(AppName);
-                    return value == null; // Returns true if the key does not exist
+                    return value == null; // اگر کلید وجود نداشته باشد، true برمی‌گرداند
                 }
             }
-            return false; // Returns false in case of an error or issue
+            return false; // در صورت خطا یا مشکل، false برمی‌گرداند
         }
 
         /// <summary>
@@ -397,27 +195,26 @@ namespace SystemAccessManager.ConsoleAppPlus
         /// </summary>
         static void CreateStartupTask()
         {
-            // Create a task in Task Scheduler to run the program with admin access on startup
+            // ایجاد وظیفه در Task Scheduler برای اجرای برنامه با دسترسی ادمین در Startup
             using (TaskService ts = new TaskService())
             {
                 TaskDefinition td = ts.NewTask();
                 td.RegistrationInfo.Description = "MyApp startup task";
 
-                // Create a trigger to run at user logon
+                // ایجاد یک تریگر برای اجرا در زمان لاگین کاربر
                 td.Triggers.Add(new LogonTrigger());
 
-                // Define an action to run your program
+                // تعیین اکشن برای اجرای برنامه شما
                 td.Actions.Add(new ExecAction(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName, null, null));
-
-                // Run the task with highest privileges
+                // اجرای وظیفه با بالاترین سطح دسترسی
                 td.Principal.RunLevel = TaskRunLevel.Highest;
 
-                // Register the task in Task Scheduler
+                // ثبت وظیفه در Task Scheduler
                 ts.RootFolder.RegisterTaskDefinition(@"MyAppStartupTask", td);
 
                 Console.WriteLine("Task created successfully.");
             }
         }
     }
-    // Author VDtem
+    // autour VDtem
 }
